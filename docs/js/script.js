1d0bc1cc55c19c5ef0fc1a6ef75a6e8538667774
Alexander Cherkas <folickthebest@gmail.com>
2020-10-28 16:30:38 +0200

    change design / end portfolio and contact section / ready to adaptive

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,7 +1,40 @@
-window.addEventListener("scroll", function () {
-  let header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
+gsap.config({
+  nullTargetWarn: false,
 });
+let slider = new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 50,
+  // centeredSlides: true,
+  // loop: true,
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+// slider.on('slideChange', function () {
+//   gsap.to('.swiper-slide-active', {
+//     scale: .85,
+//     duration: .5,
+//   });
+// });
+// slider.on('slideChangeTransitionEnd', function () {
+//   gsap.to('.swiper-slide-active', {
+//     scale: 1,
+//     ease: "power3.out",
+//     duration: .5,
+//   });
+// });
+// gsap.to('.swiper-slide', {
+//   scale: .85,
+//   duration: 0,
+// });
+// gsap.to('.swiper-slide-active', {
+//   scale: 1,
+//   duration: 0,
+// });
+
 $(function () {
   $(".menu__btn").on("click", function (e) {
     $(this).toggleClass("is-active"),
@@ -11,67 +44,18 @@ $(function () {
   $("img, a").on("dragstart", function (event) {
     event.preventDefault();
   });
-});
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-
-gsap.utils.toArray(".menu__list .menu__item .menu__link").forEach(function (a) {
-  a.addEventListener("click", function (e) {
-    e.preventDefault();
-    gsap.to(window, {
-      duration: 1.5,
-      scrollTo: e.target.getAttribute("href")
-    });
+  window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
   });
 });
-
-// function animateFrom(elem, direction) {
-//   direction = direction | 1;
-
-//   var x = 0,
-//     y = direction * 100;
-//   if (elem.classList.contains("left")) {
-//     x = -100;
-//     y = 0;
-//   } else if (elem.classList.contains("right")) {
-//     x = 100;
-//     y = 0;
-//   }
-//   gsap.fromTo(elem, {
-//     x: x,
-//     y: y,
-//     autoAlpha: 0,
-//   }, {
-//     duration: 1.5,
-//     x: 0,
-//     y: 0,
-//     autoAlpha: 1,
-//     ease: "ease-in",
-//     overwrite: "auto"
-//   });
-// }
-
-// function hide(elem) {
-//   gsap.set(elem, {
-//     autoAlpha: 0,
-//   });
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   gsap.registerPlugin(ScrollTrigger);
-
-//   gsap.utils.toArray(".anim").forEach(function (elem) {
-//     hide(elem);
-//     ScrollTrigger.create({
-//       start: 'top 99%',
-//       trigger: elem,
-//       markers: true,
-//       onEnter: function () {
-//         animateFrom(elem)
-//       },
-//       onEnterBack: function () {
-//         animateFrom(elem, 1)
-//       },
-//       onLeave: function () {}
+// gsap
+// gsap.utils.toArray(".menu__list .menu__item .menu__link").forEach(function (a) {
+//   a.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     gsap.to(window, {
+//       duration: 1.5,
+//       scrollTo: e.target.getAttribute("href")
 //     });
 //   });
 // });
@@ -79,12 +63,12 @@ gsap.utils.toArray(".menu__list .menu__item .menu__link").forEach(function (a) {
 //   y: -100,
 //   duration: 1.25,
 // });
-
+// wow
 let wow = new WOW({
   boxClass: 'anim',
   animateClass: 'anim',
-  offset: 0,
-  mobile: true,
-  live: true,
+  // offset: 0,
+  // mobile: true,
+  // live: true,
 });
 wow.init();
