d7f4f79f1a97496d50bc7e7dd080e76e8604c1e7
Alexander Cherkas <folickthebest@gmail.com>
2020-11-03 03:32:42 +0200

    end adaptive / ready to GSAP anim

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,40 +1,6 @@
 gsap.config({
   nullTargetWarn: false,
 });
-let slider = new Swiper(".swiper-container", {
-  slidesPerView: 3,
-  spaceBetween: 50,
-  // centeredSlides: true,
-  // loop: true,
-  mousewheel: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
-
-// slider.on('slideChange', function () {
-//   gsap.to('.swiper-slide-active', {
-//     scale: .85,
-//     duration: .5,
-//   });
-// });
-// slider.on('slideChangeTransitionEnd', function () {
-//   gsap.to('.swiper-slide-active', {
-//     scale: 1,
-//     ease: "power3.out",
-//     duration: .5,
-//   });
-// });
-// gsap.to('.swiper-slide', {
-//   scale: .85,
-//   duration: 0,
-// });
-// gsap.to('.swiper-slide-active', {
-//   scale: 1,
-//   duration: 0,
-// });
-
 $(function () {
   $(".menu__btn").on("click", function (e) {
     $(this).toggleClass("is-active"),
@@ -46,7 +12,9 @@ $(function () {
   });
   window.addEventListener("scroll", function () {
     let header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+    header.classList.toggle("sticky", window.scrollY > 100);
+    let menu = document.querySelector(".menu__list");
+    menu.classList.toggle("sticky", window.scrollY > 100);
   });
 });
 // gsap
@@ -58,17 +26,4 @@ $(function () {
 //       scrollTo: e.target.getAttribute("href")
 //     });
 //   });
-// });
-// gsap.from('.menu__btn, .logo', {
-//   y: -100,
-//   duration: 1.25,
-// });
-// wow
-let wow = new WOW({
-  boxClass: 'anim',
-  animateClass: 'anim',
-  // offset: 0,
-  // mobile: true,
-  // live: true,
-});
-wow.init();
+// }); 
